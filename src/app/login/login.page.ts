bc6f96821080d88fb6a26ad89dfcea807897cd29
Tiago Nunes <t92283356@gmail.com>
2021-12-18 00:46:56 -0300

    Alteracao da pagina de login, tab2 e tab3

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -18,12 +18,16 @@ export class LoginPage implements OnInit {
   }
 
   login(){
-    if(this.email === 'admin@admin.com' && this.senha === 'admin'){
+    if(this.email === '@admin' && this.senha === '12345'){
       this.route.navigateByUrl('/tabs/tab1');
       this.presentToast('Seja bem vindo!', 'success');
     }else{
       this.presentToast('ERRO! Usuário e/ou senha inválidos!', 'danger');
     }
+  }
+
+  registro(){
+    this.presentToast('ID: @admin / PW: 12345', 'success');
   }
 
   async presentToast(texto: string, cor: string) {
